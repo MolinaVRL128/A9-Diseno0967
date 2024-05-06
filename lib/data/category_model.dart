ded2a412db2f820976b37efbdc215f57c1fc4caf
REYNA_LETICIA_MOLINA_VILLELA <a.21308051280967@cbtis128.edu.mx>
2024-05-06 15:31:45 +0000

    actualiza

---
+++ lib/data/category_model.dart
@@ -0,0 +1,14 @@
+class Category {
+  final int id;
+  final String name;
+
+  Category(this.id, this.name);
+}
+
+List<Category> categories = [
+  Category(0, 'Todo'),
+  Category(1, 'Cotizar'),
+  Category(2, 'Agendar'),
+  Category(3, 'Pastel'),
+  Category(4, 'Empleados'),
+];
---
+++ lib/data/plant_data.dart
@@ -0,0 +1,76 @@
+import 'package:molina/data/plant_model.dart';
+
+List<Plants> plants = [
+  Plants(
+    id: 0,
+    name: 'Choco',
+    imagePath: 'assets/images/image1.png',
+    category: 'Pastel',
+    description:
+        'Rico pastel de 3 leches, nuestro pastel Choco esta relleno de sabores increibles, vamos compra el tuyo ya!!',
+    price: 900.00,
+    isFavorit: false,
+  ),
+  Plants(
+    id: 1,
+    name: 'Freslishus',
+    imagePath: 'assets/images/image2.png',
+    category: 'Pastel',
+    description:
+        'Nuestro increible pastel relleno de fresa es un sabor que amaras desde el primer bocado, compra el tuyo ya!!',
+    price: 600.00,
+    isFavorit: false,
+  ),
+  Plants(
+    id: 2,
+    name: 'Moris',
+    imagePath: 'assets/images/image3.png',
+    category: 'Pastel',
+    description:
+        'Nuestro famoso pastel morado, un pastel demasiado rico y hermoso, que esperas compra el tuyo ya!!',
+    price: 1300.00,
+    isFavorit: false,
+  ),
+  Plants(
+    id: 3,
+    name: 'Cumple',
+    imagePath: 'assets/images/image5.png',
+    category: 'Pastel',
+    description:
+        'Nuestro pastel mas sencillo pero sin duda el mas importante, regalale este rico pastel a la persona que quieres!!',
+    price: 500.00,
+    isFavorit: false,
+  ),
+];
+
+List<Plants> populerPlants = [
+  Plants(
+    id: 0,
+    name: 'Cremis',
+    imagePath: 'assets/images/01.png',
+    category: 'Pastel',
+    description: 'Nuestro pastel cremosisimo, vamos disfrutalo ahora mismo!!',
+    price: 655.00,
+    isFavorit: false,
+  ),
+  Plants(
+    id: 1,
+    name: 'Choco cumple',
+    imagePath: 'assets/images/02.png',
+    category: 'Pastel',
+    description:
+        'Nuestro pastel demasiado vendido debes probar este increible sabor ahora mismo!!!',
+    price: 785.00,
+    isFavorit: false,
+  ),
+  Plants(
+    id: 2,
+    name: 'Triple choco',
+    imagePath: 'assets/images/03.png',
+    category: 'Pastel',
+    description:
+        'Nuestro pastel triple chocolate sin duda lo amaras demasiado!!',
+    price: 1000.00,
+    isFavorit: false,
+  ),
+];
